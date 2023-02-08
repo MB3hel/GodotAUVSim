27664bd0ca3e9ec7b0ec57930c8ed53decb8f895
Marcus Behel <marcus.behel@mindspring.com>
2023-02-08 15:38:43 -0500

    Add local mode sim

--- robot.gd
+++ robot.gd
@@ -1,45 +1,71 @@
+## Robot script handles moving the robot itself
+## Control baord equivilent math is implemented here
+
 extends Spatial
 
 
-# Degrees per second for rotate animation
-const dps = 45.0;
+# Max speeds of robot
+var max_rotation = 0;				# Degrees per second
+var max_translation = 0;			# Translation units per second
 
-# Angle to rotate to
-const pitch = 90.0;				# About x
-const roll = 90.0;				# About y
-const yaw = 0.0;				# About z
-const target = Vector3(pitch, roll, yaw);
+
+# Current motion in robot-relative DoFs
+var curr_translation = Vector3(0.0, 0.0, 0.0);
+var curr_rotation = Vector3(0.0, 0.0, 0.0);
+
+####################################################################################################
+# Godot functions
+####################################################################################################
 
 func _ready():
-	var refrobot = get_parent().get_node("RefRobot")
-	refrobot.rotate_x(pitch * PI / 180.0);
-	refrobot.rotate_y(roll * PI / 180.0);
-	refrobot.rotate_z(yaw * PI / 180.0);
-	
-func _process(delta):
-	# Get error in degrees (diff)
-	var curr = self.rotation_degrees;
-	var diff = target - curr;
-	
-	# Determine "direction" of change (positive, negative, or zero)
-	var signs = Vector3(0, 0, 0);
-	if abs(diff.x) > 0.1:
-		signs.x = diff.x / abs(diff.x);
-	if abs(diff.y) > 0.1:
-		signs.y = diff.y / abs(diff.y);
-	if abs(diff.z) > 0.1:
-		signs.z = diff.z / abs(diff.z);
-		
-	# Determine change in degrees based on speeds
-	var deg_change = signs * dps * delta;
-	
-	# Don't overshoot
-	if abs(deg_change.x) > abs(diff.x):
-		deg_change.x = diff.x;
-	if abs(deg_change.y) > abs(diff.y):
-		deg_change.y = diff.y;
-	if abs(deg_change.z) > abs(diff.z):
-		deg_change.z = diff.z
+	pass
 
-	var new = curr + deg_change;
-	self.rotation_degrees = new;
+
+func _process(delta):
+	# Performs actual motion (LOCAL mode implementation)
+	var rotation_change = curr_rotation * (max_rotation * PI / 180.0) * delta;
+	var translation_change = curr_translation * max_translation * delta;
+	self.translate_object_local(translation_change);
+	self.rotate_object_local(Vector3(1, 0, 0), rotation_change.x)
+	self.rotate_object_local(Vector3(0, 1, 0), rotation_change.y)
+	self.rotate_object_local(Vector3(0, 0, 1), rotation_change.z)	
+
+####################################################################################################
+
+
+####################################################################################################
+# Motor control functions
+####################################################################################################
+
+# Motor control speed set in LOCAL mode (equivilent of control board LOCAL mode)
+func mc_set_local(x: float, y: float, z: float, pitch: float, roll: float, yaw: float):
+
+	x = limit(x, -1.0, 1.0)
+	y = limit(y, -1.0, 1.0)
+	z = limit(z, -1.0, 1.0)
+	pitch = limit(pitch, -1.0, 1.0)
+	roll = limit(roll, -1.0, 1.0)
+	yaw = limit(yaw, -1.0, 1.0)
+	curr_translation.x = x
+	curr_translation.y = y
+	curr_translation.z = z
+	curr_rotation.x = pitch
+	curr_rotation.y = roll
+	curr_rotation.z = yaw
+
+####################################################################################################
+
+
+####################################################################################################
+# Helper functions
+####################################################################################################
+
+func limit(v: float, lower: float, upper: float) -> float:
+	if v > upper:
+		return upper
+	if v < lower:
+		return lower
+	return v
+	
+####################################################################################################
+
---
+++ simulation.gd
@@ -0,0 +1,24 @@
+## Top level simulation control script
+## Attach to root node in scene
+
+extends Spatial
+
+####################################################################################################
+# Godot node functions
+####################################################################################################
+
+onready var robot = get_node("Robot")
+
+func _ready():
+	# Setup robot parameters
+	robot.max_rotation = 75
+	robot.max_translation = 1
+	
+	# Test move the robot in LOCAL mode
+	#                   x    y    z    p    r    y
+	robot.mc_set_local(0.0, 0.0, 0.0, 0.0, 0.0, 0.0)
+
+func _process(delta):
+	pass
+	
+####################################################################################################
