fcd1b7a5a3eb57760c6892d2f6d616855f08f387
Marcus Behel <marcus.behel@mindspring.com>
2023-02-08 18:59:42 -0500

    Add multiple viewports / cameras

--- simulation.gd
+++ simulation.gd
@@ -16,7 +16,7 @@ func _ready():
 	
 	# Test move the robot in LOCAL mode
 	#                   x    y    z    p    r    y
-	robot.mc_set_local(0.0, 0.0, 0.0, 0.0, 0.0, 0.0)
+	robot.mc_set_local(0.0, 0.0, 0.1, 0.0, 0.0, 0.0)
 
 func _process(delta):
 	pass
