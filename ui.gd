7c53ab7afc9c5359a1665688c1052434b76ba857
Marcus Behel <marcus.behel@mindspring.com>
2023-02-09 11:39:37 -0500

    Adjust status ui format and font to prevent clipping

--- ui.gd
+++ ui.gd
@@ -14,28 +14,20 @@ onready var quat_label = get_node("VBoxContainer/HBoxContainer/VBoxContainer/Sta
 onready var copy_button = get_node("VBoxContainer/HBoxContainer/VBoxContainer/CopyButton")
 
 
-const translation_template = "(x={0}, y={1}, z={2})"
-const rotation_template = "(p={0}, r={1}, y={2})"
-const euler_template = "(p={0}, r={1}, y={2})"
-const quat_template = "(w={0}, x={1}, y={2}, z={3})"
+const translation_template = "(x=%+.2f, y=%+.2f, z=%+.2f)"
+const rotation_template = "(p=%+.2f, r=%+.2f, y=%+.2f)"
+const euler_template = "(p=%+.2f, r=%+.2f, y=%+.2f)"
+const quat_template = "(w=%+.4f, x=%+.4f, y=%+.4f z=%+.4f)"
 
 
 func _ready():
 	copy_button.connect("pressed", self, "copy_to_clipboard")
 
 func _process(_delta):
-	translation_label.text = translation_template.format(
-		[curr_translation.x, curr_translation.y, curr_translation.z]
-	)
-	rotation_label.text = rotation_template.format(
-		[curr_rotation.x, curr_rotation.y, curr_rotation.z]
-	)
-	euler_label.text = euler_template.format(
-		[robot_euler.x, robot_euler.y, robot_euler.z]
-	)
-	quat_label.text = quat_template.format(
-		[robot_quat.w, robot_quat.x, robot_quat.y, robot_quat.z]
-	)
+	translation_label.text = translation_template % [curr_translation.x, curr_translation.y, curr_translation.z]
+	rotation_label.text = rotation_template % [curr_rotation.x, curr_rotation.y, curr_rotation.z]
+	euler_label.text = euler_template % [robot_euler.x, robot_euler.y, robot_euler.z]
+	quat_label.text = quat_template % [robot_quat.w, robot_quat.x, robot_quat.y, robot_quat.z]
 
 func copy_to_clipboard():
 	var info = "Local Translation: {0}\r\nLocal Rotation: {1}\r\nEuler Orientation: {2}\r\nQuaternion Orientation: {3}\r\n"
