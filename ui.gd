5c18fc8fbf2d20e1ddc37efb750075bb3192294e
Marcus Behel <marcus.behel@mindspring.com>
2023-02-08 21:50:06 -0500

    Implement copy status text to clipboard

--- ui.gd
+++ ui.gd
@@ -7,10 +7,11 @@ var robot_euler = Vector3(0.0, 0.0, 0.0)
 var robot_quat = Quat(0.0, 0.0, 0.0, 1.0)
 
 # UI elements
-onready var translation_label = get_node("VBoxContainer/HBoxContainer/StatusPanel/TranslationValue")
-onready var rotation_label = get_node("VBoxContainer/HBoxContainer/StatusPanel/RotationValue")
-onready var euler_label = get_node("VBoxContainer/HBoxContainer/StatusPanel/EulerValue")
-onready var quat_label = get_node("VBoxContainer/HBoxContainer/StatusPanel/QuatValue")
+onready var translation_label = get_node("VBoxContainer/HBoxContainer/VBoxContainer/StatusPanel/TranslationValue")
+onready var rotation_label = get_node("VBoxContainer/HBoxContainer/VBoxContainer/StatusPanel/RotationValue")
+onready var euler_label = get_node("VBoxContainer/HBoxContainer/VBoxContainer/StatusPanel/EulerValue")
+onready var quat_label = get_node("VBoxContainer/HBoxContainer/VBoxContainer/StatusPanel/QuatValue")
+onready var copy_button = get_node("VBoxContainer/HBoxContainer/VBoxContainer/CopyButton")
 
 
 const translation_template = "(x={0}, y={1}, z={2})"
@@ -20,7 +21,7 @@ const quat_template = "(w={0}, x={1}, y={2}, z={3})"
 
 
 func _ready():
-	pass
+	copy_button.connect("pressed", self, "copy_to_clipboard")
 
 func _process(_delta):
 	translation_label.text = translation_template.format(
@@ -35,3 +36,10 @@ func _process(_delta):
 	quat_label.text = quat_template.format(
 		[robot_quat.w, robot_quat.x, robot_quat.y, robot_quat.z]
 	)
+
+func copy_to_clipboard():
+	var info = "Local Translation: {0}\r\nLocal Rotation: {1}\r\nEuler Orientation: {2}\r\nQuaternion Orientation: {3}\r\n"
+	info = info.format(
+		[translation_label.text, rotation_label.text, euler_label.text, quat_label.text]
+	)
+	OS.set_clipboard(info)
