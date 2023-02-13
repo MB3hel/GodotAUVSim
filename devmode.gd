0e59c4bbeb6bbfc678a5d2e80936214f874f768d
Marcus Behel <marcus.behel@mindspring.com>
2023-02-13 12:28:30 -0500

    Add better vector method to devmode

--- devmode.gd
+++ devmode.gd
@@ -34,9 +34,42 @@ var t = Timer.new()
 
 func _ready():
 	sim.reset_sim()
-	var cboard_rot = Vector3(0.0, 180.0, 0.0);
+	var cboard_rot = Vector3(110.0, 0.0, 0.0);
 	robot.rotation = Angles.cboard_euler_to_godot_euler(cboard_rot * PI / 180.0);
 	print("Orientation: ", Angles.godot_euler_to_cboard_euler(robot.rotation) * 180.0 / PI)
+	
+	
+	var q = Angles.godot_euler_to_quat(robot.rotation)
+	var vgm = Vector3(0, 0, 0)
+	vgm.x = 2.0 * (-q.x*q.z + q.w*q.y)
+	vgm.y = 2.0 * (-q.w*q.x - q.y*q.z)
+	vgm.z = -q.w*q.w + q.x*q.x + q.y*q.y - q.z*q.z
+	
+	var vgm_xz = project_xz(vgm)
+	var vgm_yz = project_yz(vgm)
+	
+	print(vgm_yz)
+	print(vgm_xz)
+	
+	var rollerr = 0.0
+	var pitcherr = 0.0
+	
+	if vgm_xz.length() < 0.8:
+		rollerr = 0.0
+	else:
+		rollerr = acos(vgm_xz.dot(Vector3(0, 0, -1)))
+	
+	if vgm_yz.length() < 0.8:
+		pitcherr = 0.0
+	else:
+		pitcherr = acos(vgm_yz.dot(Vector3(0, 0, -1)))
+	
+	# TODO: Determine how to handle rotations past 90
+	
+	print(pitcherr * 180.0 / PI)
+	print(rollerr * 180.0 / PI)
+	
+	return
 	
 	t.one_shot = false
 	t.connect("timeout", self, "dothings")
@@ -47,16 +80,17 @@ func _process(delta):
 	pass
 
 func dothings():
-	var q = Angles.godot_euler_to_quat(robot.rotation)
-	var vgm = Vector3(0, 0, 0)
-	vgm.x = 2.0 * (-q.x*q.z + q.w*q.y)
-	vgm.y = 2.0 * (-q.w*q.x - q.y*q.z)
-	vgm.z = -q.w*q.w + q.x*q.x + q.y*q.y - q.z*q.z
-	
-	# Note: Singularity at pitch / roll of 90 because vgm.z == 0
-	# Also works wrong for pitch / roll of 180
-	var scale = -1.0 / vgm.z
-	var vdir = Vector3(vgm.y, vgm.x, 0.0) * scale
-	
-	print(vdir)
+	pass
 
+func project_yz(v: Vector3) -> Vector3:
+	 # Project v onto yz plane
+	var n = Vector3(1, 0, 0)
+	var u = v - ((v.dot(n) / n.length_squared()) * n)
+	return u.normalized()
+
+
+func project_xz(v: Vector3) -> Vector3:
+	 # Project v onto yz plane
+	var n = Vector3(0, 1, 0)
+	var u = v - ((v.dot(n) / n.length_squared()) * n)
+	return u.normalized()
