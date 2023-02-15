bf673efb0e1d7f2cac363f4c06be70b24b9c8013
Marcus Behel <marcus.behel@mindspring.com>
2023-02-15 14:47:32 -0500

    Added sassist todo comments

--- devmode.gd
+++ devmode.gd
@@ -33,7 +33,7 @@ func should_hijack():
 var t = Timer.new()
 
 func _ready():
-	var cboard_rot = Vector3(0.0, 0.0, 0.0)
+	var cboard_rot = Vector3(0.0, 0.0, 90.0)
 	robot.rotation = Angles.cboard_euler_to_godot_euler(cboard_rot * PI / 180.0)
 	t.one_shot = false
 	t.connect("timeout", self, "dothings")
@@ -45,9 +45,10 @@ func _process(delta):
 
 
 var delaycount = 0.0
-var enable_yaw_control = false
+var enable_yaw_control = true
 
-var target_euler = Vector3(45.0, 45.0, 180.0)
+# TODO Construct this differently so that roll and yaw are what they mean in GLOBAL mode of operation
+var target_euler = Vector3(15.0, 0.0, 90.0)
 var first = false
 
 func dothings():
@@ -63,14 +64,16 @@ func dothings():
 	if not first:
 		print("Target Quat: (w = %.4f, x = %.4f, y = %.4f, z = %.4f" % [qt.w, qt.x, qt.y, qt.z])
 		first = true
-	
+
+
 	if not enable_yaw_control:
 		var e = Angles.quat_to_cboard_euler(q)
 		var et = Angles.quat_to_cboard_euler(qt)
 		et.z = e.z
 		q = Angles.cboard_euler_to_quat(e)
 		qt = Angles.cboard_euler_to_quat(et)
-	
+
+
 	var qrot = diff_quat(q, qt).normalized()
 	var qv = Vector3(qrot.x, qrot.y, qrot.z)
 	var qv_mag = qv.length()
@@ -78,31 +81,34 @@ func dothings():
 	var e_mag = 2.0 * atan(qv_mag / qr)
 	var e = e_mag * qv
 	
+	# TODO: Rotate e by rotation matrix from q to localize it
+	
+	
 	var pitch_speed = 0.0
 	var roll_speed = 0.0
 	var yaw_speed = 0.0
 	
-	pitch_speed = 16.0 * (-e.x);
+	pitch_speed = 4.0 * (-e.x);
 	pitch_speed = 1.0 if pitch_speed > 1.0 else pitch_speed
 	pitch_speed = -1.0 if pitch_speed < -1.0 else pitch_speed
 
-	roll_speed = 16.0 * (-e.y);
+	roll_speed = 4.0 * (-e.y);
 	roll_speed = 1.0 if roll_speed > 1.0 else roll_speed
 	roll_speed = -1.0 if roll_speed < -1.0 else roll_speed
 
 	if enable_yaw_control:
-		yaw_speed = 16.0 * (-e.z)
+		yaw_speed = 4.0 * (-e.z)
 		yaw_speed = 1.0 if yaw_speed > 1.0 else yaw_speed
 		yaw_speed = -1.0 if yaw_speed < -1.0 else yaw_speed
 	
 	if is_nan(pitch_speed) or is_nan(roll_speed) or is_nan(yaw_speed):
 		print("BROKE")
 		return
-		
+	
 	
 	cboard.motor_wdog_feed()
 	cboard.mode = cboard.MODE_GLOBAL
-	cboard.mc_set_global(0, 0, 0, pitch_speed, roll_speed, yaw_speed, q)
+	cboard.mc_set_local(0, 0, 0, pitch_speed, roll_speed, yaw_speed)
 
 
 # Signed right hand angle between a and b in the plane to which n is normal
