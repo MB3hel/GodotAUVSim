f4403b15b4486aecc38cec87744e286f23f8a128
Marcus Behel <marcus.behel@mindspring.com>
2023-02-15 01:36:11 -0500

    Alt method in devmode

--- devmode.gd
+++ devmode.gd
@@ -47,7 +47,7 @@ func _process(delta):
 var delaycount = 0.0
 var enable_yaw_control = true
 
-var target_euler = Vector3(15.0, 180.0, 170.0)
+var target_euler = Vector3(15.0, 120.0, 90.0)
 var first = false
 
 func dothings():
@@ -65,44 +65,58 @@ func dothings():
 		first = true
 	
 	var qrot = diff_quat(q, qt).normalized()
-	var angle = acos(qrot.w) * 2.0
-	var axis = Vector3(0.0, 0.0, 0.0)
-	if angle != 0.0:
-		axis.x = qrot.x / sin(angle / 2.0)
-		axis.y = qrot.y / sin(angle / 2.0)
-		axis.z = qrot.z / sin(angle / 2.0)
-	angle = restrict_angle_deg(angle * 180.0 / PI)
-	var angular_velocity = angle * axis.normalized()
-	var e = angular_velocity
+	
+	
+	# var angle = acos(qrot.w) * 2.0
+	# var axis = Vector3(0.0, 0.0, 0.0)
+	# if angle != 0.0:
+	# 	axis.x = qrot.x / sin(angle / 2.0)
+	# 	axis.y = qrot.y / sin(angle / 2.0)
+	# 	axis.z = qrot.z / sin(angle / 2.0)
+	# angle = restrict_angle_deg(angle * 180.0 / PI)
+	# var angular_velocity = angle * axis.normalized()
+	# var e = angular_velocity
+	# NOTE: Signs on e are not always what would be expected
+	# Example: (both start at 0, 0, 0)
+	# Target of 0, 120, 0 requires negative e.y (-x, +z)
+	# Target of 15, 180, 170 requires positive e.y (-x, +z)
+	# Perhaps an issue because of the 180 specifically???
+	# Also 15, 120, 0 just doesn't work at all??? Very wrong signs??? No clue.
+	# Target of 15, 120, 0 requires (+x, -y, +z)
+	# The "shortest path" motion also doesn't seem to work quite right in cases with multiple axes of motion
+	# It seems to move away then back most of the time. Possibly releated. Possibly not. Possibly just bad tuning
+	
+	# Alt e vector??? Maybe works??? Maybe doesn't???
+	var qv = Vector3(qrot.x, qrot.y, qrot.z)
+	var qv_mag = qv.length()
+	var qr = qrot.w
+	var e_mag = 2.0 * atan(qv_mag / qr)
+	var e = e_mag * qv
 	
 	var pitch_speed = 0.0
 	var roll_speed = 0.0
 	var yaw_speed = 0.0
 	
-	pitch_speed = 0.03 * (-e.x);
+	pitch_speed = 16.0 * (-e.x);
 	pitch_speed = 1.0 if pitch_speed > 1.0 else pitch_speed
 	pitch_speed = -1.0 if pitch_speed < -1.0 else pitch_speed
 
-	roll_speed = 0.03 * (e.y);
+	roll_speed = 16.0 * (-e.y);
 	roll_speed = 1.0 if roll_speed > 1.0 else roll_speed
 	roll_speed = -1.0 if roll_speed < -1.0 else roll_speed
 
-	yaw_speed = 0.03 * (e.z)
+	yaw_speed = 16.0 * (-e.z)
 	yaw_speed = 1.0 if yaw_speed > 1.0 else yaw_speed
 	yaw_speed = -1.0 if yaw_speed < -1.0 else yaw_speed
 	
 	if is_nan(pitch_speed) or is_nan(roll_speed) or is_nan(yaw_speed):
 		print("BROKE")
-		print("%.32f" % qrot.w)
-		print(angle)
-		print(axis)
-		print()
 		return
 		
 	
 	cboard.motor_wdog_feed()
 	cboard.mode = cboard.MODE_LOCAL
-	cboard.mc_set_local(0, 0, 0, pitch_speed, roll_speed, yaw_speed)
+	cboard.mc_set_global(0, 0, 0, pitch_speed, roll_speed, yaw_speed, q)
 
 
 # Signed right hand angle between a and b in the plane to which n is normal
