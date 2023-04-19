a05274e9bef1c2aa727207c3b6dda4ee3e2a243e
Marcus Behel <marcus.behel@mindspring.com>
2023-04-19 01:54:15 -0400

    Clarify some simcb comments

--- simcb.gd
+++ simcb.gd
@@ -11,13 +11,21 @@
 #   - Test code to communicate with the control board (messages and format)
 # However there are some limitations (which is why using a real control board 
 # is supported at all)
-#   - Does not allow testing of math under "real conditions. Meaning single
-#     precision floating point math andcorrect timings.
+#   - Does not allow testing of math under "real" conditions. Mostly related to
+#     timing and CPU load conditions.
 #   - Does not allow testing of firmware bugs. Ex: issues in firmware math 
 #     libraries, firmware crashes, firmware deadlocks
-#   - Does not handle some commands that configure a physical system. Ex:
-#     thruster inversion, IMU axis config, motor matrix commands, 
-#     RAW mode speed set.
+#
+# Note that this implementation is not the most efficient dataflow. The simcb
+# is meant to be a "drop in" replacement for a real control board.
+# As such, messages are formatted here before they are given to the cboard
+# layer. This means the cboard layer technically performs some unnecessary
+# parsing. Likewise, the netiface parses a message, the it is written raw
+# to simcb only to be parsed again. This is not the most efficient method of
+# doing things, however it ensures that cboard is almost identacal for a real
+# control board or with simcb. It also means that no simcb logic or data
+# data handling need to be in cboard. This makes the simulator more maintainable
+# and shouldn't impact simulator performance too much.
 ################################################################################
 
 extends Node
